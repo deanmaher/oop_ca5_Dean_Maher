ea00a7c742bdc555d5dde29fbacd9edec94cab62
deanmaher <72203596+deanmaher@users.noreply.github.com>
2021-03-22 23:21:55 +0000

    Course Choice Manager Business Object added

--- src/main/java/com/dkit/oopca5/client/BusinessObjects/App.java
+++ src/main/java/com/dkit/oopca5/client/BusinessObjects/App.java
@@ -26,27 +26,215 @@ package com.dkit.oopca5.client.BusinessObjects;
 
 
 
+import com.dkit.oopca5.server.DAOs.CourseDaoInterface;
+import com.dkit.oopca5.server.DAOs.StudentDaoInterface;
+import com.dkit.oopca5.server.DAOs.MySqlCourseDao;
+import com.dkit.oopca5.server.DAOs.MySqlStudentDao;
+import com.dkit.oopca5.server.DAOs.MySqlDao;
 import com.dkit.oopca5.server.DTOs.Student;
+import com.dkit.oopca5.server.DTOs.Course;
+import com.dkit.oopca5.server.Exceptions.DaoException;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class App {
+    public static void main(String[] args) throws IOException, ParseException {
 
-    public static void main(String[] args) {
+        System.out.println("CAO Online - CA5 - Dean Maher SD2A");
+        new App().start();
+    }
+
+    private void start() throws IOException, ParseException {
+
 
         // load students
         StudentManager studentManager = new StudentManager();
+
+        // load courses
+        CourseManager courseManager = new CourseManager();
+
+
+//        CourseChoicesManager mgr = new CourseChoicesManager(studentManager, courseManager);
+        boolean systemOn;
+
+        CourseDaoInterface courseDao = new MySqlCourseDao();
+
+
+        while (systemOn = true) {
+            Scanner keyboard = new Scanner(System.in);
+            int type = firstMenu();
+            try {
+                if (type == 0) {
+
+                    System.out.println("Thank you for using this system. Goodbye!");
+                    break;
+                }
+                if (type == 1) {
+//                    register?????
+
+                }
+                if (type == 2) {
+//                    normal login
+//                    printMainMenu();
+                    mainMenu();
+
+                } else {
+                    System.out.println("Please enter either 0, 1 or 2");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e + " Please Enter a NUMBER [0-2]");
+            }
+        }
+
+
+    }
+
+    public static void printMainMenu() {
+        System.out.println("\n**************************************************************");
+
+        System.out.println("0. QUIT");
+        System.out.println("1. LOGOUT");
+        System.out.println("2. DISPLAY_COURSE");
+        System.out.println("3. DISPLAY_ALL_COURSES");
+        System.out.println("4. DISPLAY_CURRENT_CHOICES");
+        System.out.println("5. UPDATE_CURRENT_CHOICES");
+
+    }
+
+    public int firstMenu() {
+        int option1 = 0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("0. QUIT_APPLICATION");
+        System.out.println("1. REGISTER");
+        System.out.println("2. LOGIN");
+        return option1 = keyboard.nextInt();
+    }
+
+    public void mainMenu() throws IOException {
+        StudentManager studentManager = new StudentManager();
+        CourseManager courseManager = new CourseManager();
+        CourseChoicesManager mgr = new CourseChoicesManager(studentManager, courseManager);
+        Scanner keyboard = new Scanner(System.in);
+
+        //For student menu login
+
+
+        System.out.println("Please enter CAO Number:");
+        int caoNumber = keyboard.nextInt();
+
+
+        System.out.println("Please enter date of birth (DD/MM/YYYY):");
+        String dob = keyboard.next();
+
+        System.out.println("Please enter password:");
+        String password = keyboard.next();
+        mgr.login(caoNumber, dob, password);
+        Student student = mgr.getStudentDetails(caoNumber);
+        System.out.println("Student: " + student);
+        printMainMenu();
+
+        System.out.print("Please enter option:");
+        int option = keyboard.nextInt();
+        while (option <= 5)
+        {
+            try
+            {
+
+                if (option == 0)
+                {
+//                    quit
+                   break;
+
+                }
+                else if (option == 1)
+                {
+//                 logout
+                }
+                else if (option == 2)
+                {
+//                    display course
+//                    System.out.println("Displaying all courses: ");
+//                    System.out.println(mgr.getAllCourses());
+
+                    pressAnyKeyToContinue();
+
+                }
+                else if (option == 3)
+                {
+//                 DISPLAY ALL COURSES
+
+                    CourseDaoInterface courseDao = new MySqlCourseDao();
+                    try
+                    {
+                        List<Course> courseList = courseDao.findAllCourses();
+                        System.out.println(courseList);
+                    }
+                    catch (DaoException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+                else if (option == 4)
+                {
+        //                 DISPLAY COURSE
+
+//                    CourseDaoInterface courseDao = new MySqlCourseDao();
+//                    try
+//                    {
+//                        List<Course> courseList = courseDao.findCourseByID();
+//                        System.out.println(courseList);
+//                    }
+//                    catch (DaoException e)
+//                    {
+//                        e.printStackTrace();
+//                    }
+
+                }
+
+                }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Please Enter a number [0-5]");
+            }
+
+
+//    public static void main(String[] args) {
 //
-//        Student s = studentManager.getStudent(12345678);
-
-        int caoNumber = 11111111;
-        String dob = "1999-09=09";
-        String password = "password";
-        studentManager.addStudent(new Student (caoNumber, dob, password));
-
-        Student s = studentManager.getStudent(11111111);
-        System.out.println("Getting student with this caoNumber: "+s);
+//        // load students
+//        StudentManager studentManager = new StudentManager();
+////
+////        Student s = studentManager.getStudent(12345678);
+//
+//        int caoNumber = 11111111;
+//        String dob = "1999-09=09";
+//        String password = "password";
+//        studentManager.addStudent(new Student (caoNumber, dob, password));
+//
+//        Student s = studentManager.getStudent(11111111);
+//        System.out.println("Getting student with this caoNumber: "+s);
+//    }
+        }
+    }
+    public void pressAnyKeyToContinue()
+    {
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
     }
 }
+
 
 
 
---
+++ src/main/java/com/dkit/oopca5/client/BusinessObjects/CourseChoicesManager.java
@@ -0,0 +1,103 @@
+package com.dkit.oopca5.client.BusinessObjects;
+
+import com.dkit.oopca5.server.DTOs.Course;
+import com.dkit.oopca5.server.DTOs.Student;
+
+import java.util.*;
+
+public class CourseChoicesManager {
+
+    // reference to constructor injected studentManager
+    private StudentManager studentManager;
+
+    // reference to constructor injected courseManager
+    private CourseManager courseManager;
+
+    // Store all the Course details - HashMap for fast access
+    // Map [courseId => course]
+    // Loaded in constructor from CoursesManager and converted from List
+    private Map<String, Course> coursesMap = new HashMap<>();
+
+    // caoNumber, course selection list - HashMap for fast access
+    // courses must be in List, to maintain the selected order.
+    private Map<Integer, List<Course>> studentChoicesMap = new HashMap<>();
+
+    // CourseChoicesManager DEPENDS on both the StudentManager and CourseManager to access
+    // student details and course details.  So, we pass in a reference to each via
+    // the constructor.
+    // This is called "Dependency Injection", meaning that we
+    // inject (or pass in) objects that this class requires to do its job.
+    //
+    CourseChoicesManager(StudentManager studentManager, CourseManager courseManager) {
+        this.studentManager = studentManager;
+        this.courseManager = courseManager;
+
+        // studentChoicesMap - load from file, caoNumber, courseId1, courseId2, etc....
+
+        List<Course> coursesList = courseManager.getAllCourses(); // get list of courses
+
+        // Iterate over the List and populate the Map  //  Old way (before Java 8) - but easier to understand than streams
+        for (Course course : coursesList) {
+            coursesMap.put(course.getCourseId(), course);
+        }
+
+        // new way with streams and lambdas // toMap() returns HashMap by default
+        //        coursesMap =  coursesList.stream()
+        //                .collect(Collectors.toMap(Course::getCourseId, course -> course));
+
+    }
+
+    Student getStudentDetails(int caoNumber) {
+        return studentManager.getStudent(caoNumber);
+    }
+
+    Course getCourseDetails(String courseId) {
+        return courseManager.getCourse(courseId);
+    }
+
+    List<Course> getStudentChoices(int caoNumber) {
+        return studentChoicesMap.get(caoNumber);    // to be sent to Web Interface normally
+    }
+
+    void updateChoices(int caoNumber, List<String> choiceList) { // data from web interface normally
+        // choiceList is a list of courseId, as Strings.
+        // List used as it preserves choice order.
+        // studentChoices map requires course objects...
+        // look up course object using courseId
+        // add course to a list
+
+        ArrayList<Course> coursesList = new ArrayList<>();
+
+        for (String courseId : choiceList) {
+            Course course = coursesMap.get(courseId);
+            coursesList.add(course);
+
+        }
+        // finally, put the list of courses in the map using caoNumber as key
+        studentChoicesMap.put(caoNumber, coursesList);
+    }
+
+    // Returns a *List* of Courses.
+    // Courses are not in any particular order.
+    // (would be sent to Web interface normally )
+    public List<Course> getAllCourses() {
+
+        ArrayList<Course> list = new ArrayList<>(); // new ArrayList to store copy of Map data
+
+        // Iterate through all values in the courseMap, and add each course to the List
+        for (Map.Entry<String, Course> entry : coursesMap.entrySet()) {
+            Course course = entry.getValue();   // get course from map entry
+            list.add(course);                   // add course to the List
+        }
+        return list;    // list of courses, in no particular order.
+    }
+
+    boolean login(int coaNumber,String dateOfBirth,String password) {
+        //if(  dob and password match the caoNumber)
+        return true;
+        // else
+        //  return false;
+    }
+    // RegEx for creation of new students and courses
+
+}
