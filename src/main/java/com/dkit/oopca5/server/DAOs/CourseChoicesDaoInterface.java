51773531d0b4d62999580329ab364680afdc1157
deanmaher <72203596+deanmaher@users.noreply.github.com>
2021-03-23 00:04:28 +0000

    Course Choice Manager Business Object added

---
+++ src/main/java/com/dkit/oopca5/server/DAOs/CourseChoicesDaoInterface.java
@@ -0,0 +1,8 @@
+package com.dkit.oopca5.server.DAOs;
+import com.dkit.oopca5.server.DTOs.Course;
+import com.dkit.oopca5.server.Exceptions.DaoException;
+import java.util.List;
+
+public interface CourseChoicesDaoInterface {
+    public List<String> getStudentChoices() throws DaoException;
+}
---
+++ src/main/java/com/dkit/oopca5/server/DAOs/MySqlCourseChoicesDao.java
@@ -0,0 +1,74 @@
+package com.dkit.oopca5.server.DAOs;
+
+
+import com.dkit.oopca5.server.DTOs.Course;
+import com.dkit.oopca5.server.DTOs.Student;
+import com.dkit.oopca5.server.Exceptions.DaoException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlCourseChoicesDao extends MySqlDao implements CourseChoicesDaoInterface {
+
+    @Override
+    public List<String> getStudentChoices() throws DaoException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<String> choices = new ArrayList<>();
+
+        try
+        {
+            //Get connection object using the methods in the super class (MySqlDao.java)...
+            con = this.getConnection();
+
+            String query = "SELECT * FROM STUDENT_COURSES WHERE ";
+            ps = con.prepareStatement(query);
+
+            //Using a PreparedStatement to execute SQL...
+            rs = ps.executeQuery();
+            while (rs.next())
+            {
+                String caoNumber = rs.getString("caoNumber");
+                String courseID = rs.getString("courseID");
+                String order = rs.getString("order");
+                 Course c = new Course(caoNumber, courseID, order);
+                courses.add(c);
+            }
+        } catch (SQLException e)
+        {
+            throw new DaoException("findAllCourses() " + e.getMessage());
+        } finally
+        {
+            try
+            {
+                if (rs != null)
+                {
+                    rs.close();
+                }
+                if (ps != null)
+                {
+                    ps.close();
+                }
+                if (con != null)
+                {
+                    freeConnection(con);
+                }
+            } catch (SQLException e)
+            {
+                throw new DaoException("findAllCourses() " + e.getMessage());
+            }
+        }
+        return courses;     // may be empty
+    }
+
+
+}
+
+
+
